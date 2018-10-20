3ec3d4132b62dd1425a8ce5614ee45e64223e0fc
Holben888 <holben888@gmail.com>
2018-10-20 17:28:52 -0400

    add intro page, assets, grid background

---
+++ js/intro.js
@@ -0,0 +1,10 @@
+const screens = {
+  title: `
+    <img id="logo" src="res/FINGRR.svg" />
+    <button class="title">Start your adventure</button>
+  `
+}
+
+const container = document.getElementById('container')
+
+container.innerHTML = screens['title']
